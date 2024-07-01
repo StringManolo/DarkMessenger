c2b25ce513523a671582a3a0c4c25b8ddbc8d458
StringManolo <manuelvarelacaldas@gmail.com>
2024-07-01 18:03:35 +0000

    Add tor start and tor stop

--- node/index.js
+++ node/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { promises as fs } from "fs";
+import fs from "fs";
 // import path from 'path';
 import { spawn } from "child_process";
 import parseCLI from "simpleargumentsparser";
@@ -31,7 +31,7 @@ stop      shutdown all services
   if (cli.c.start || cli.o[0].includes("start")) {
     await start(cli);
   } else if (cli.c.stop  || cli.o[0].includes("stop")) {
-    stop();
+    await stop(cli);
   }
 
 })();
@@ -58,17 +58,20 @@ const error = msg => {
 }
 
 const startTor = () => {
+  debug(`Creating file to store tor pid process ... `);
+
   verbose(`Starting Tor ...`);
-  const process = spawn("/usr/bin/tor",  ["-f", "./config/torrc.conf" ]);
-    
-  process.stdout.on('data', (data) => {
-    debug(`Tor stdout: ${data}`);
+  const process = spawn("/usr/bin/tor",  ["-f", "./config/torrc.conf"], {
+    detached: true,
+    stdio: "ignore"
   });
+ 
+  debug(`Storing tor pid at ./tor_files/tor.pid ...`);
+  fs.writeFileSync("./tor_files/tor.pid", process.pid.toString());
 
-  process.stderr.on('data', (data) => {
-    error(`Tor stderr: ${data}`);
-  });
-
+  debug(`Detaching tor process from node process ...`);
+  process.unref();
+  
   process.on('error', (err) => {
     error(`Error Starting Tor: ${err}`);
   });
@@ -79,6 +82,28 @@ const startTor = () => {
   });
 }
 
+const stopTor = () => {
+  debug(`Extracting tor process id from ./tor_files/tor.pid ... `);
+  if (fs.existsSync("./tor_files/tor.pid")) {
+
+    const pid = parseInt(fs.readFileSync("./tor_files/tor.pid").toString(), 10);
+    debug(`Extracted pid: ${pid}`);
+
+    try {
+      verbose(`Stopping Tor`);
+      debug(`Sending SIGTERM signal tor process id ${pid}`);
+      process.kill(pid, 'SIGTERM');
+      console.log("Tor sucessfully stopped.");
+      debug(`Deleting ./tor_files/tor.pid file ...`);
+      fs.unlinkSync("./tor_files/tor.pid");
+      debug(`./tor_files/tor.pid has been deleted`);
+    } catch (err) {
+      error(`Unable to terminate tor process with PID ${pid}: ${err}`);
+    }
+  } else {
+    error(`./tor_files/tor.pid can't be found`);
+  }
+}
 
 const start = async (cli) => {
   if (cli.c.config) {
@@ -99,16 +124,39 @@ const start = async (cli) => {
   verbose("Verbose Activated");
   debug("Debug Activated");
 
+  debug(`Calling startTor() ... `);
   startTor();
+  debug(`startTor() call done.`);
 }
 
-const stop = () => {
-  console.log(`Stopping...`);
+const stop = async (cli) => {
+  if (cli.c.config) {
+    if (typeof cli.c.config === "string") {
+      config = await loadConfig(cli.c.config);
+    }
+  }
+
+
+  if (!config) {
+    console.log(`Loading default config file expected at (./config/dark-messenger.json) ... `);
+    config = await loadConfig("./config/dark-messenger.json");
+    if (!config) {
+      exit(`Unable to load any config.`);
+    }
+  }
+
+  verbose("Verbose Activated");
+  debug("Debug Activated");
+
+  console.log(`Stopping All Services...`);
+  debug(`Calling stopTor() ... `);
+  stopTor();
+  debug(`stopTor() call done.`);
 }
 
 const loadFile = async (path) => {
   try {
-    const data = await fs.readFile(path, "utf8");
+    const data = await fs.promises.readFile(path, "utf8");
     return data;
   } catch (err) {
     error(`Unable to read ${path}: ${err}`)
